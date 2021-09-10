b98d33dc5f8a5404270e47e37b6375e584cd7d73
th1ran13 <thiraniherath@gmail.com>
2021-09-10 20:19:31 +0530

    nav-bar-with-dropdown

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Tutorials',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Become a Tutor',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  }
+  // {
+  //   title: 'Design',
+  //   path: '/design',
+  //   cName: 'dropdown-link'
+  // },
+  // {
+  //   title: 'Development',
+  //   path: '/development',
+  //   cName: 'dropdown-link'
+  // }
+];
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+
+export default function SignUp() {
+  return(
+    <>
+    {/* <SignIn/> */}
+    <Footer/>
+  </>
+     
+  );
+}
